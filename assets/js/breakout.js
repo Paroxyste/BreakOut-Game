91c9de070fc8e339f2ce9816c9bb4e06d8962cdb
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-01-14 17:57:49 +0100

    Add keyboard & StartBtn structure func

---
+++ assets/js/breakout.js
@@ -0,0 +1,18 @@
+let zoneGame = document.querySelector('#zoneGame');
+let ball = document.querySelector('#ball');
+let paddle = document.querySelector('#paddle');
+let btn_start = document.querySelector('.startBtn');
+
+btn_start.addEventListener('click', startGame);
+
+document.addEventListener('keyleft', function(e) {
+
+});
+
+document.addEventListener('keyright', function(e) {
+
+});
+
+function startGame() {
+
+}
