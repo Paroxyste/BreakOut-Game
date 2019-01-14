3315ef4084fdad4b29804489b6187e6f0e972fb6
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-01-14 21:05:56 +0100

    Declare all global variable

--- assets/js/breakout.js
+++ assets/js/breakout.js
@@ -3,6 +3,15 @@ let ball = document.querySelector('#ball');
 let paddle = document.querySelector('#paddle');
 let btn_start = document.querySelector('.startBtn');
 
+let gameOver = false;
+let gameInPlay = false;
+let score = 0;
+let lives = 3;
+
+let animRepeat;
+let ballDir = [5, 5, 5];
+let containerDim = container.getBoundingClientRect();
+
 btn_start.addEventListener('click', startGame);
 
 document.addEventListener('keyleft', function(e) {
