f38ae5f7f01e5a2be8ccd25b4f0a9810e7a3618b
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-01-16 11:56:59 +0100

    Fix & Refactoring Code

--- assets/js/breakout.js
+++ assets/js/breakout.js
@@ -1,14 +1,15 @@
 let container = document.querySelector('.container');
 let ball = document.querySelector('#ball');
-let paddle = document.querySelector('#paddle');
+let paddle = document.querySelector('.paddle');
 let btn_start = document.querySelector('.startBtn');
 
 let gameOver = true;
 let gameInPlay = false;
-let score = 0;
-let lives = 3;
 
-let animRepeat;
+let score = 0;
+let lives = 5;
+
+let animationRepeat;
 let ballDir = [5, 5, 5];
 let containerDim = container.getBoundingClientRect();
 
@@ -20,7 +21,7 @@ document.addEventListener('keydown', function(e) {
 
   if(key === 37) paddle.left = true;
   else if(key === 39) paddle.right = true;
-  else if(key === 38) && !gameInPlay) gameInPlay = true;
+  else if(key === 38 && !gameInPlay) gameInPlay = true;
 });
 
 document.addEventListener('keyup', function(e) {
@@ -38,159 +39,159 @@ function startGame() {
     lives = 3;
     setupBricks(24);
     lifeUpdater();
-    animRepeat = requestAnimationFrame(update);
+    animationRepeat = requestAnimationFrame(update);
     gameOver = false;
     gameInPlay = false;
-  }
-}
+  };
+};
 
 function setupBricks(num) {
   let row = {
-    x: ((containerDim.width % 100) /2),
+    x: ((containerDim.width % 100) / 2),
     y: 50
   };
 
-  for(let x = 0, x < num; x++) {
+  for(let x = 0; x < num; x++) {
     if(row.x > (containerDim.width - 100)) {
-       row.y += 70;
-       row.x = ((containerDim.width % 100) / 2);
-    }
+        row.y += 70;
+        row.x = ((containerDim.width % 100) / 2);
+    };
+
     brickMaker(row);
     row.x += 100;
-  }
-}
+  };
+};
 
 function brickMaker(row) {
   let div = document.createElement('div');
       div.setAttribute('class', 'brick');
-      div.style.background = 'linear-gradient('+ ranColor() + ',' ranColor() +')';
-      let pointDiv = Math.ceil(Math.random() * 10) + 2;
+      div.style.background = 'linear-gradient(' + ranColor() + ', #ddd)';
+
+  let pointDiv = Math.ceil(Math.random() * 10) + 2;
       div.dataset.points = pointDiv;
-      div.innerHTML = pontDiv;
+      div.innerHTML = pointDiv;
       div.style.left = row.x + 'px';
       div.style.top = row.y + 'px';
       container.appendChild(div);
-}
+};
 
 function ranColor() {
-  function color() {
+  function c() {
     let hex = Math.floor(Math.random() * 256).toString(16);
     let response = ('0' + String(hex)).substr(-2);
     return response;
-  }
+  };
   return '#' + c() + c() + c();
-}
+};
 
 function update() {
   if(gameOver === false) {
     let pCurrent = paddle.offsetLeft;
-    if(paddle.left && pCurrent > 0) {
-      pCurrent -= 5;
-    } else if(paddle.right && pCurrent < (containerDim.width - paddle.offsetWidth)) {
-      pCurrent += 5;
-    }
+
+    if(paddle.left && pCurrent > 0) pCurrent -= 5;
+    else if(paddle.right && pCurrent < (containerDim.width - paddle.offsetWidth)) pCurrent += 5;
 
     paddle.style.left = pCurrent + 'px';
-    if(!gameInPlay) {
-      waitingOnPaddle();
-    } else {
-      ballMove();
-    }
-    animRepeat = requestAnimationFrame(update);
-  }
-}
+
+    if (!gameInPlay) waitingOnPaddle();
+    else ballMove();
+
+    animationRepeat = requestAnimationFrame(update);
+  };
+};
 
 function waitingOnPaddle() {
   ball.style.top = (paddle.offsetTop - 22) + 'px';
   ball.style.left = (paddle.offsetLeft + 70) + 'px';
-}
+};
 
 function ballMove() {
   let x = ball.offsetLeft;
   let y = ball.offsetTop;
 
-  if(x > (containerDim.width - 20) || x < 0) {
-    ballDir[0] *= -1;
-  }
-
+  if(x > (containerDim.width - 20) || x < 0) ballDir[0] *= -1;
   if(y > (containerDim.height - 20) || y < 0) {
-    if(y > (containerDim.height - 20)) {
+    if (y > (containerDim.height - 20)) {
       fallOffEdge();
       return;
-    }
-    ballDir[1] *= 1;
-  }
+    };
+
+    ballDir[1] *= -1;
+  };
 
   if(isCollide(ball, paddle)) {
     let nDir = ((x - paddle.offsetLeft) - (paddle.offsetWidth / 2)) / 10;
-    ballDir[0] = nDir;
-    ballDir[1] *= -1;
-  }
+      ballDir[0] = nDir;
+      ballDir[1] *= -1;
+  };
 
   let tempBricks = document.querySelectorAll('.brick');
   if(tempBricks.length == 0) {
     stopper();
     setupBricks(20);
-  }
+  };
 
-  for(let targetBrick of tempBricks) {
-    if(isCollide(targetBrick, ball)) {
+  for(var tarBrick of tempBricks) {
+    if(isCollide(tarBrick, ball)) {
       ballDir[1] *= -1;
-      targetBrick.parentNode.removeChild(targetBrick);
-      scoreUpdate(targetBrick.dataset.points);
-    }
-  }
+      tarBrick.parentNode.removeChild(tarBrick);
+      scoreUpdate(tarBrick.dataset.points);
+    };
+  };
 
   x += ballDir[0];
   y += ballDir[1];
+
   ball.style.top = y + 'px';
   ball.style.left = x + 'px';
-}
+};
 
 function lifeUpdater() {
-  document.querySelector('lives').innerText = lives;
-}
+  document.querySelector('.lives').innerText = lives;
+};
 
 function scoreUpdate(num) {
   score += parseInt(num);
   document.querySelector('.score').innerText = score;
-}
+};
 
 function stopper() {
   gameInPlay = false;
   ballDir[0, -5];
   waitingOnPaddle();
-  window.cancelAnimationFrame(animRepeat);
-}
+  window.cancelAnimationFrame(animationRepeat);
+};
 
 function endGame() {
   document.querySelector('.gameover').style.display = 'block';
-  document.querySelector('.gameover').innerHTML = 'GAME OVER<br>Your Score' + score;
+  document.querySelector('.gameover').innerHTML = 'GAME OVER<br>Your Score ' + score;
   gameOver = true;
   ball.style.display = 'none';
 
   let tempBricks = document.querySelectorAll('.brick');
-  for(var targetBrick of tempBricks) {
-    targetBrick.parentNode.removeChild(targetBrick);
-  }
-}
+  for(let tarBrick of tempBricks) {
+    tarBrick.parentNode.removeChild(tarBrick);
+  };
+};
 
 function fallOffEdge() {
   lives--;
+
   if(lives < 0) {
     endGame();
     lives = 0;
-  }
+  };
+
   lifeUpdater();
   stopper();
-}
+};
 
 function isCollide(a, b) {
   let aRect = a.getBoundingClientRect();
   let bRect = b.getBoundingClientRect();
 
-  return (!(aRect.bottom < bRect.top ||
+  return (!(aRect.bottom < bRect.top || 
             aRect.top > bRect.bottom ||
             aRect.right < bRect.left ||
             aRect.left > bRect.right));
-}
+};
